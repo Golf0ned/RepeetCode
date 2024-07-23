4bf6cbd8be5258753729f6d184abccb3c461bbc8
Golf0ned <benjaminye.email@gmail.com>
2024-07-22 22:55:36 -0700

    move run

--- src/repeetcode.py
+++ src/repeetcode.py
@@ -2,6 +2,7 @@ import csv
 
 def load_data(filename):
     data = []
+    # a+ to generate file if not existing
     with open(filename, 'a+', newline='') as csv_file:
         reader = csv.reader(csv_file)
         for row in reader:
