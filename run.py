514e664562223e54a7b8d813ca42e6235fab242d
Golf0ned <benjaminye.email@gmail.com>
2024-07-22 22:34:32 -0700

    finish runner

--- run.py
+++ run.py
@@ -1,12 +1,29 @@
 import csv
-import sys
+import os
+
+from src import repeetcode
 
 filename = "data.csv"
 
-print('Reading data...', end='')
-data = []
-with open(filename, newline='') as csv_file:
-    reader = csv.reader(csv_file)
-    for row in reader:
-        data.append()
-print('done!')
+
+def run():
+    data = []
+    with open(filename, newline='') as csv_file:
+        reader = csv.reader(csv_file)
+        for row in reader:
+            data.append(row)
+
+    user_input = ''
+    while user_input != 'q':
+        print('Input an option:')
+        user_input = input('[q]uit | [a]dd problem | [r]andom problem\n').lower()
+        os.system('cls' if os.name=='nt' else 'clear')
+        match user_input:
+            case 'q': print('bye bye!')
+            case 'a': repeetcode.add_problem()
+            case 'r': repeetcode.random_problem()
+            case _: print('Invalid input')
+
+
+if __name__ == '__main__':
+    run()
---
+++ src/repeetcode.py
@@ -0,0 +1,8 @@
+import csv
+
+
+def add_problem():
+    print('add_problem')
+
+def random_problem():
+    print('random_problem')
