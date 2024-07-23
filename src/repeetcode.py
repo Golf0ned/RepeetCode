c8081ffa722bbada0673fdcaa416f30b04e8e06b
Golf0ned <benjaminye.email@gmail.com>
2024-07-22 22:40:01 -0700

    move file reading outside

--- src/repeetcode.py
+++ src/repeetcode.py
@@ -1,8 +1,15 @@
 import csv
 
+def load_data(filename):
+    data = []
+    with open(filename, 'a+', newline='') as csv_file:
+        reader = csv.reader(csv_file)
+        for row in reader:
+            data.append(row)
+    return data
 
-def add_problem():
+def add_problem(data):
     print('add_problem')
 
-def random_problem():
+def random_problem(data):
     print('random_problem')
